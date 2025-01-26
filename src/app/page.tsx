14aa71ab535a105b1f7e307ef3ed3353f90ef11a
Spandan-Mishra <spandanmishra69@gmail.com>
2025-01-26 11:44:44 +0530

    timeline added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Prizes from "@/components/nupur/prizes";
 import Gallery from "@/components/anwesha/gallary/gallery";
 import Sponsorship from "@/components/anwesha/sponsorship/sponser";
 import FAQ from "@/components/anwesha/faq/faq";
+import Timeline from "@/components/spandan/timeline/timeline";
 export default function Home() {
   return (
    <>
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <About />
       <Sponsorship/>
+      <Timeline />
       <Prizes />
       <Gallery/>
       <FAQ/>
