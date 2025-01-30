79d56faff459957d7e3a5724a19321975a4ac93a
Nupur <nrmcodes22@gmail.com>
2025-01-30 18:40:43 +0530

    font changes

--- src/components/nupur/navbar/page.tsx
+++ src/components/nupur/navbar/page.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
               <a className="text-white hover:text-blue-500">About AU</a>
             </Link>
             <a
-              href="#"
+              href="#stats"
               className="text-white hover:text-blue-500"
             >
               Stats 2024
@@ -45,7 +45,7 @@ export default function Navbar() {
               Gallery
             </a>
             <a
-              href="#"
+              href="#faq"
               className="text-white hover:text-blue-500"
             >
               FAQs
@@ -53,12 +53,7 @@ export default function Navbar() {
           </div>
 
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Sign in
-            </button>
+            
 
             {/* Hamburger Menu for Mobile */}
             <div className="md:hidden">
@@ -105,7 +100,7 @@ export default function Navbar() {
                     href="#sponsers"
                     className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
-                    Sponsers
+                    Sponsors
                   </a>
                 </li>
                 <li>
