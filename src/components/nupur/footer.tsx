eac774e390dabd3e219d1e4bc6bfb29ca14e9383
Nupur <nrmcodes22@gmail.com>
2025-02-01 23:04:28 +0530

    footer changes

--- src/components/nupur/footer.tsx
+++ src/components/nupur/footer.tsx
@@ -19,19 +19,33 @@ export default function Footer() {
                    height={60} alt="Flowbite Logo" className=""/>
                 
             </div>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 justify-center md:justify-end ">
+            <ul className="flex flex-wrap items-center gap-1  justify-center md:justify-end ">
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                <a href="https://www.linkedin.com/authwall?trk=bf&trkInfo=AQGvsFXvbjcTMwAAAZQ2VrZAylI432X-Rc8T6sdGT0z3OVZnNOlOu6lxpREcdp0nNYpai9gPBU-HpMdBWt5pwc-HH7nHfxUVJkhs5mv_RZczxWMc4JwFH2pT2KC6vBnEJA5ZLCE=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Faps-nitr%2F" className="hover:underline me-4 md:me-6" target="_blank">
+                    <Image src="/images/linkedin.svg" width={30}   height={30} alt="Flowbite Logo" className=""/>
+                    </a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    <a href="https://www.instagram.com/aps.nitr/" className="hover:underline me-4 md:me-6" target="_blank">
+                    <Image src="/images/instagram.svg" width={30}   height={30} alt="Flowbite Logo" className=""/>
+                    </a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                <a href="https://www.threads.net/@aps.nitr" className="hover:underline me-4 md:me-6" target="_blank">
+                    <Image src="/images/threads.svg" width={30}   height={30} alt="Flowbite Logo" className=""/>
+                    </a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline">Contact</a>
+                <a href="https://www.youtube.com/@APSNITR" className="hover:underline me-4 md:me-6" target="_blank">
+                    <Image src="/images/yt.svg" width={30}   height={30} alt="Flowbite Logo" className=""/>
+                    </a>
                 </li>
+                <li>
+                <a href="https://x.com/ApsocietyNitr?mx=24" className="hover:underline me-4 md:me-6" target="_blank">
+                    <Image src="/images/x.svg" width={30}   height={30} alt="Flowbite Logo" className=""/>
+                    </a>
+                </li>
+                
             </ul>
         </div>
         
