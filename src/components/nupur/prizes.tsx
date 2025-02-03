d8ee2fa44d2717146abc67460487359b39d1ab4f
Nupur <nrmcodes22@gmail.com>
2025-02-03 11:40:19 +0530

    prizes

--- src/components/nupur/prizes.tsx
+++ src/components/nupur/prizes.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
+import Image from "next/image";
 export default function About() {
   return (
    <>
@@ -17,48 +18,100 @@ export default function About() {
         <div className="mx-auto mb-8 max-w-screen-md  lg:mb-12 text-center">
             <h2 className="mb-10 text-4xl font-bold tracking-tight text-white">Takeaways</h2>
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-center gap-4 ">
-      <div className="bg-transparent border border-white text-white p-6 rounded-xl">
-      <h1 className="text-center text-2xl mb-2 font-bold text-gray-400 ">Team Contest</h1>
-      <ul className="text-left font-montserrat text-gray-400 list-disc list-inside space-y-2 leading-relaxed  md:text-base">
-      
-      <li className="flex flex-col gap-1 pl-1">
-    <span className="font-semibold text-lg md:text-xl">WINNERS 🥇</span> 
-    <span>Rs. <span className="font-semibold text-lg md:text-xl">39,000</span></span>
-  </li>
-  <li className="flex flex-col gap-1 pl-1">
-    <span className="font-semibold text-lg md:text-xl">1st RUNNERS UP 🥈</span> 
-    <span>Rs. <span className="font-semibold text-lg md:text-xl">32,000</span></span>
-  </li>
-  <li className="flex flex-col gap-1 pl-1">
-    <span className="font-semibold text-lg md:text-xl">2nd RUNNERS UP 🥉</span> 
-    <span>Rs. <span className="font-semibold text-lg md:text-xl">23,000</span></span>
-  </li>
-        </ul> 
-      </div>
-      <div className="hidden lg:block h-[250px] w-0.5 bg-neutral-200 dark:bg-gray-600"></div>
-      <div className="bg-transparent border border-white text-white p-6 rounded-xl">
-      <h1 className="text-center text-2xl mb-2 font-bold text-gray-400 ">Individual Contest</h1>
-      <ul className="text-left font-montserrat text-gray-400 list-disc list-inside space-y-2 leading-relaxed  md:text-base">
-      
-      <li className="flex flex-col gap-1 pl-1">
-    <span className="font-semibold text-lg md:text-xl">WINNER 🥇</span> 
-    <span>Rs. <span className="font-semibold text-lg md:text-xl">24,000</span></span>
-  </li>
-  <li className="flex flex-col gap-1 pl-1">
-    <span className="font-semibold text-lg md:text-xl">1st RUNNERS UP 🥈</span> 
-    <span>Rs. <span className="font-semibold text-lg md:text-xl">19,000</span></span>
-  </li>
-  <li className="flex flex-col gap-1 pl-1">
-    <span className="font-semibold text-lg md:text-xl">2nd RUNNERS UP 🥉</span> 
-    <span>Rs. <span className="font-semibold text-lg md:text-xl">13,000</span></span>
-  </li>
-        </ul> 
+        
+<div className="flex items-center justify-center">
+   
+    <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+       
+        <div className="space-y-8  lg:grid md:grid-cols-2 lg:space-y-0 lg:gap-10">
+           
+            <div
+                className="mx-auto flex max-w-lg space-y-8 items-start flex-col rounded-3xl border border-gray-200 bg-gray-300
+ p-6 text-gray-800 shadow-[0_0_20px_5px_rgba(59,130,246,0.6)]
+                 xl:p-8">
+
+                <h2 className="text-2xl font-semibold ">Team Contest</h2>
+                
+                
+                <ul role="list" className="mb-8 space-y-4 text-left text-gray-800  text-sm">
+                    <li className="flex items-center space-x-3 ">
+                       <Image src="/images/1st-prize.png" width={40}
+                                          height={40} alt="Flowbite Logo" className=""/>
+                        
+                        <span className="text-lg font-semibold">Winners : 39000 INR </span>
+                    </li>
+                    <li className="flex items-center space-x-3">
+                    <Image src="/images/2nd-place.png" width={40}
+                                          height={40} alt="Flowbite Logo" className=""/>
+                        
+                        <span className="text-lg font-semibold">1st Runners Up : 32000 INR </span>
+                        
+                        
+                    </li>
+                    <li className="flex items-center space-x-3">
+                        
+                    <Image src="/images/3rd-place.png" width={40}
+                                          height={40} alt="Flowbite Logo" className=""/>
+                        
+                        <span className="text-lg font-semibold">2nd Runners Up : 23000 INR </span>
+                    </li>
+                    <li className="flex items-center space-x-3">
+                        
+                    <Image src="/images/hoodie.png" width={40}
+                                          height={40} alt="Flowbite Logo" className=""/>
+                        
+                        <span className="text-lg font-semibold">AlgoUtsav Exclusive Merch for every participant.</span>
+                    </li>
+                    
+                </ul>
+
+            </div>
+            
+            <div
+                className="mx-auto flex max-w-lg space-y-8 items-start flex-col rounded-3xl  bg-cyan-300 p-6 text-gray-900 xl:p-8 shadow-[0_0_20px_5px_rgba(59,130,246,0.6)]">
+                    
+                    <h2 className="text-2xl font-semibold ">Individual Contest</h2>
+                
+                
+                <ul role="list" className="mb-8 space-y-4 text-left text-gray-900  text-sm">
+                    <li className="flex items-center space-x-3 ">
+                       <Image src="/images/1st-prize.png" width={40}
+                                          height={40} alt="Flowbite Logo" className=""/>
+                        
+                        <span className="text-lg font-semibold">Winners : 24000 INR </span>
+                    </li>
+                    <li className="flex items-center space-x-3">
+                    <Image src="/images/2nd-place.png" width={40}
+                                          height={40} alt="Flowbite Logo" className=""/>
+                        
+                        <span className="text-lg font-semibold">1st Runners Up : 19000 INR </span>
+                        
+                        
+                    </li>
+                    <li className="flex items-center space-x-3">
+                        
+                    <Image src="/images/3rd-place.png" width={40}
+                                          height={40} alt="Flowbite Logo" className=""/>
+                        
+                        <span className="text-lg font-semibold">2nd Runners Up : 13000 INR </span>
+                    </li>
+                    <li className="flex items-center space-x-3">
+                        
+                    <Image src="/images/hoodie.png" width={40}
+                                          height={40} alt="Flowbite Logo" className=""/>
+                        
+                        <span className="text-lg font-semibold">AlgoUtsav Exclusive Merch for every participant. </span>
+                    </li>
+                </ul>
+
+            </div>
+            
+            
         </div>
-    </div> 
-      
     </div>
-    <div className="bg-transparent border border-white text-gray-300 text-xl p-6 rounded-xl">Algoutsav Exclusive Merch will be given along with the prizes to all the participants.</div>    
+   
+    </div>
+        </div>
     
     </motion.div>
     </div>
